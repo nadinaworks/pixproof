2639593f352f13e732318383bfd0c620bcf142a3
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-03-18 16:20:49 +0200

    added styles

--- views/pixproof_gallery.php
+++ views/pixproof_gallery.php
@@ -1,4 +1,4 @@
-<ul id="pixproof_gallery" class="mosaic  mosaic--masonry">
+<ul id="pixproof_gallery" class="mosaic  mosaic--masonry  nav  nav--stacked">
 	<?php foreach ( $attachs as $attachment ) { ?>
 		<li class="proof_photo mosaic__item">
 			<img src="<?php echo $attachment->guid ?>" alt=""/>
