47c5ae5adf571c74ef20dbeabdc522868a1c30a9
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-03-19 10:52:36 +0200

    selected class + gitignore

--- views/pixproof_gallery.php
+++ views/pixproof_gallery.php
@@ -9,34 +9,27 @@
  */
 
 ?>
-<div id="pixproof_gallery" class="mosaic  mosaic--masonry  cf">
+<div id="pixproof_gallery" class="gallery  gallery-columns-3  cf">
 	<?php foreach ( $attachs as $attachment ) { 
 
-        $thumb_img = wp_get_attachment_image_src($attachment->ID, 'full-size');
+        // $thumb_img = wp_get_attachment_image_src($attachment->ID, 'full-size');
 
-        $thumb_img_ratio = 70; //some default aspect ratio in case something has gone wrong and the image has no dimensions - it happens
-        if (isset($thumb_img[1]) && isset($thumb_img[2]) && $thumb_img[1] > 0) {
-            $thumb_img_ratio = $thumb_img[2] * 100/$thumb_img[1];
-        }
+        // $thumb_img_ratio = 70; //some default aspect ratio in case something has gone wrong and the image has no dimensions - it happens
+        // if (isset($thumb_img[1]) && isset($thumb_img[2]) && $thumb_img[1] > 0) {
+        //     $thumb_img_ratio = $thumb_img[2] * 100/$thumb_img[1];
+        // }
 
 		?>
-		<div class="proof-photo  mosaic__item">
-            <div class="mosaic__item-container">
-                <div class="mosaic__image" style="padding-top: <?php echo $thumb_img_ratio; ?>%">
-                    <img src="<?php echo $attachment->guid ?>" alt="<?php echo $attachment->post_title; ?>">
-                </div>
-                <div class="mosaic__meta">
-                    <div class="mosaic__hoverdir">
-                        <div class="flexbox">
-                            <div class="flexbox__item">
-                            	<a class="meta__action  zoom-action" href="#">Zoom</a>
-								<hr class="separator">
-								<a class="meta__action  select-action" href="#">Select</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+		<div class="proof-photo  gallery-item">
+            <img src="<?php echo $attachment->guid ?>" alt="<?php echo $attachment->post_title; ?>">
+            <ul class="actions-nav">
+            	<li>
+        			<a class="meta__action  zoom-action" href="#">Zoom</a>
+            	</li>
+            	<li>
+					<a class="meta__action  select-action" href="#">Select</a>
+            	</li>
+            </ul>
 			<span class="proof-photo__id">
 				#<?php echo $attachment->ID; ?>
 			</span>            
