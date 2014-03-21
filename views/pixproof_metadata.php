0e71884a3114625b2a71fa9a8bc825c109d24c49
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-03-21 10:45:31 +0200

    finised default styling

--- views/pixproof_metadata.php
+++ views/pixproof_metadata.php
@@ -12,7 +12,7 @@
 <div id="pixproof_data" class="pixproof-data">
 	<div class="grid"><!--
 		<?php if ( !empty($client_name)) { ?>
-			--><div class="grid__item  lap-and-up-one-quarter">
+			--><div class="grid__item  one-half  lap-and-up-one-quarter  push-half--bottom">
 	            <div class="entry__meta-box">
 	                <span class="meta-box__title">Client</span>
 	                <span><?php echo $client_name; ?></span>
@@ -20,7 +20,7 @@
 			</div><!--
 		<?php }
 		if ( !empty($event_date)) { ?>
-		--><div class="grid__item  lap-and-up-one-quarter">
+		--><div class="grid__item  one-half  lap-and-up-one-quarter  push-half--bottom">
 	            <div class="entry__meta-box">
 	                <span class="meta-box__title">Event date</span>
 	                <span><?php echo $event_date; ?></span>
@@ -28,7 +28,7 @@
 			</div><!--
 		<?php }
 		if ( !empty($number_of_images)) { ?>
-		--><div class="grid__item  lap-and-up-one-quarter">
+		--><div class="grid__item  one-half  lap-and-up-one-quarter">
 	            <div class="entry__meta-box">
 	                <span class="meta-box__title">Images</span>
 	                <span><?php echo $number_of_images; ?></span>
@@ -36,7 +36,7 @@
 			</div><!--
 		<?php }
 		if ( !empty( $file ) ) { ?>
-			--><div class="grid__item  lap-and-up-one-quarter">
+			--><div class="grid__item  one-half  lap-and-up-one-quarter">
 				<div class="entry__meta-box">
 	                <button class="button-download  js-download" onclick="window.open('<?php echo $file; ?>')">Download</button>
 	            </div>
